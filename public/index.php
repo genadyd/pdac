50e200572a994d8810d72a38ac4af0ea7804fa8d
genady <jobgenady@gmail.com>
2021-02-16 14:17:37 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Genady
+ * Date: 2/16/21
+ * Time: 1:01 PM
+ */
+echo 'gfdgffgdfgd';
---
+++ public/router.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Genady
+ * Date: 2/16/21
+ * Time: 1:02 PM
+ */
